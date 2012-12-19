210199af8b4daf5b14627c90ddd088b37ea12cad
Tom Payne <twpayne@gmail.com>
2012-12-19 20:11:30 +0100

    Factor out igc2czml

--- igc2czml.py
+++ igc2czml.py
@@ -12,6 +12,46 @@ B_RECORD_RE = re.compile(r'^B(\d{2})(\d{2})(\d{2})(\d{2})(\d{5})([NS])(\d{3})(\d
 HFDTE_RECORD_RE = re.compile(r'^HFDTE(\d{2})(\d{2})(\d{2})')
 HFPLT_RECORD_RE = re.compile(r'^HFPLT[^:]*:(.*)$')
 
+def igc2czml(id, f, geometry=None):
+    cartographicDegrees = []
+    epoch = None
+    date = None
+    for line in f:
+        m = B_RECORD_RE.match(line)
+        if m:
+            time = datetime.time(*map(int, m.group(1, 2, 3)))
+            dt = datetime.datetime.combine(date, time)
+            if epoch is None:
+                epoch = dt
+            latitude = int(m.group(4)) + int(m.group(5)) / 60000.0
+            if m.group(6) == 'S':
+                latitude = -latitude
+            longitude = int(m.group(7)) + int(m.group(8)) / 60000.0
+            if m.group(9) == 'S':
+                longitude = -longitude
+            height = int(m.group(11)) or int(m.group(12))
+            cartographicDegrees.extend([(dt - epoch).seconds, longitude, latitude, height])
+            continue
+        m = HFDTE_RECORD_RE.match(line)
+        if m:
+            date = datetime.date(2000 + int(m.group(3)), int(m.group(2)), int(m.group(1)))
+            continue
+        m = HFPLT_RECORD_RE.match(line)
+        if m:
+            id = m.group(1).strip()
+            continue
+    result = {
+        'id': id,
+        'availability': '%s/%s' % (epoch.strftime('%Y-%m-%dT%H:%M:%SZ'), dt.strftime('%Y-%m-%dT%H:%M:%SZ')),
+        'position': {
+            'epoch': epoch.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'cartographicDegrees': cartographicDegrees,
+        },
+    }
+    if geometry:
+        result.update(geometry)
+    return result
+
 
 def main(argv):
     option_parser = OptionParser()
@@ -32,44 +72,7 @@ def main(argv):
         }
     czml = []
     for arg in args:
-        cartographicDegrees = []
-        epoch = None
-        id = os.path.basename(arg)
-        date = None
-        for line in open(arg):
-            m = B_RECORD_RE.match(line)
-            if m:
-                time = datetime.time(*map(int, m.group(1, 2, 3)))
-                dt = datetime.datetime.combine(date, time)
-                if epoch is None:
-                    epoch = dt
-                latitude = int(m.group(4)) + int(m.group(5)) / 60000.0
-                if m.group(6) == 'S':
-                    latitude = -latitude
-                longitude = int(m.group(7)) + int(m.group(8)) / 60000.0
-                if m.group(9) == 'S':
-                    longitude = -longitude
-                height = int(m.group(11)) or int(m.group(12))
-                cartographicDegrees.extend([(dt - epoch).seconds, longitude, latitude, height])
-                continue
-            m = HFDTE_RECORD_RE.match(line)
-            if m:
-                date = datetime.date(2000 + int(m.group(3)), int(m.group(2)), int(m.group(1)))
-                continue
-            m = HFPLT_RECORD_RE.match(line)
-            if m:
-                id = m.group(1).strip()
-                continue
-        o = {
-            'id': id,
-            'availability': '%s/%s' % (epoch.strftime('%Y-%m-%dT%H:%M:%SZ'), dt.strftime('%Y-%m-%dT%H:%M:%SZ')),
-            'position': {
-                'epoch': epoch.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                'cartographicDegrees': cartographicDegrees,
-            },
-        }
-        o.update(geometry)
-        czml.append(o)
+        czml.append(igc2czml(os.path.basename(arg), open(arg), geometry=geometry))
 
     if options.output in (None, '-'):
         json.dump(czml, sys.stdout, indent=options.indent, sort_keys=options.sort_keys)
